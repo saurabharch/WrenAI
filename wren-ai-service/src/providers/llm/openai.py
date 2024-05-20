c718809ba4d3234217b848e11609aece5eea6a59
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2024-05-21 01:35:07 +0800

    update

--- wren-ai-service/src/providers/llm/openai.py
+++ wren-ai-service/src/providers/llm/openai.py
@@ -15,6 +15,7 @@ from src.providers.loader import provider
 
 logger = logging.getLogger("wren-ai-service")
 
+OPENAI_BASE_URL = "https://api.openai.com/v1"
 GENERATION_MODEL_NAME = "gpt-3.5-turbo"
 GENERATION_MODEL_KWARGS = {
     "temperature": 0,
@@ -42,9 +43,11 @@ class OpenAILLMProvider(LLMProvider):
     def __init__(
         self,
         api_key: Secret = Secret.from_env_var("OPENAI_API_KEY"),
-        base_url: str = os.getenv("OPENAI_BASE_URL"),
-        generation_model: str = os.getenv("OPENAI_GENERATION_MODEL")
-        or GENERATION_MODEL_NAME,
+        base_url: str = os.getenv("OPENAI_BASE_URL") or OPENAI_BASE_URL,
+        generation_model: str = os.getenv("GENERATION_MODEL") or GENERATION_MODEL_NAME,
+        embedding_model: str = os.getenv("EMBEDDING_MODEL") or EMBEDDING_MODEL_NAME,
+        embedding_model_dim: int = int(os.getenv("EMBEDDING_MODEL_DIMENSION", 0))
+        or EMBEDDING_MODEL_DIMENSION,
     ):
         def _verify_api_key(api_key: str, base_url: str) -> None:
             """
@@ -57,37 +60,43 @@ class OpenAILLMProvider(LLMProvider):
         self._api_key = api_key
         self._base_url = base_url
         self._generation_model = generation_model
+        self._embedding_model = embedding_model
+        self._embedding_model_dim = embedding_model_dim
 
     def get_generator(
         self,
         model_kwargs: Optional[Dict[str, Any]] = GENERATION_MODEL_KWARGS,
         system_prompt: Optional[str] = None,
     ):
+        def _get_generation_kwargs(
+            model_kwargs: Optional[Dict[str, Any]] = GENERATION_MODEL_KWARGS,
+            base_url: str = OPENAI_BASE_URL,
+        ):
+            if base_url == OPENAI_BASE_URL:
+                return model_kwargs
+            elif model_kwargs != GENERATION_MODEL_KWARGS:
+                return model_kwargs
+            return None
+
         return CustomOpenAIGenerator(
             api_key=self._api_key,
             model=self._generation_model,
             system_prompt=system_prompt,
-            generation_kwargs=model_kwargs,
+            generation_kwargs=_get_generation_kwargs(model_kwargs, self._base_url),
         )
 
-    def get_text_embedder(
-        self,
-        model_name: str = EMBEDDING_MODEL_NAME,
-        model_dim: int = EMBEDDING_MODEL_DIMENSION,
-    ):
+    def get_text_embedder(self):
         return OpenAITextEmbedder(
             api_key=self._api_key,
-            model=model_name,
-            dimensions=model_dim,
+            api_base_url=self._base_url,
+            model=self._embedding_model,
+            dimensions=self._embedding_model_dim,
         )
 
-    def get_document_embedder(
-        self,
-        model_name: str = EMBEDDING_MODEL_NAME,
-        model_dim: int = EMBEDDING_MODEL_DIMENSION,
-    ):
+    def get_document_embedder(self):
         return OpenAIDocumentEmbedder(
             api_key=self._api_key,
-            model=model_name,
-            dimensions=model_dim,
+            api_base_url=self._base_url,
+            model=self._embedding_model,
+            dimensions=self._embedding_model_dim,
         )
